50d108b4d866c034bee873b3759234dd3981eccf
Sergeev34429 <149881683+Sergeev34429@users.noreply.github.com>
2024-02-24 12:17:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
-items = ('стекло', 'бумага', 'пластик')
-items_time = ('1000 лет', '1,5 месяца', '200 лет')
+items = ('стекло', 'бумага', 'пластик', 'пищевые отходы', 'картон', 'дерево', 'окурки', 'резиновые покрышки', 'алюминий', 'фольга')
+items_time = ('1000 лет!!', '1,5 месяца', '200 лет', 'от 2 недель', '2 месяца', 'до 3 лет', 'до 5 лет', '40 лет', 'до 500 лет', '100 лет')
 
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    print(f'Мы вошли в систему к {bot.user}.')
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f'Привет! Я бот {bot.user}!\n Я умею рассказывать, сколько разлагается тот или иной бытовой предмет.')
+    await ctx.send(f'Привет! Я бот {bot.user}!\nЯ умею рассказывать, сколько разлагается тот или иной бытовой предмет.\n(я не хочу это делать)')
 
 @bot.command()
 async def list(ctx):
@@ -36,4 +36,4 @@ async def time(ctx, item: str):
 async def helper(ctx):
     await ctx.send(f'Вот какие команды я умею выполнять:\n!hello - приветствие \n!helper - помощь - то, что ты сейчас читаешь \n!list - список бытовых предметов, которые я знаю \n!time (название предмета) - выводит время разложения введённого бытового предмета')
 
-bot.run("MTIwMjYyNzk4MzA0NzkyMTY4NQ.GanG4s.YKals6v7a2RBwphPUXt5MLofzBxXMkT9u9gxsA") 
+bot.run("1MTIwMjYyNzk4MzA0NzkyMTY4NQ.G79K-T.jWalv-qtFehWmlNq1SqaPVHFs4m11mwC9p65PM") 
